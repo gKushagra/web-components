0bf63bd3b7f1742afd958823d6fe1c139b205bbf
gKushagra <55290773+gKushagra@users.noreply.github.com>
2022-12-27 20:05:11 -0500

    fixed webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,8 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
 module.exports = {
-    mode: 'production',
-    entry: {
-        'web-components': [
-            './src/components/index.js',
-            './src/components/sidebar.js'
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                exclude: /[\\/]node_modules[\\/]/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-        ]
+    "mode": "none",
+    "entry": "./src/components/index.js",
+    "output": {
+        "path": __dirname + '/dist',
+        "filename": "main.js"
     }
 };
