4245a81ed2b1eeb7aa14799d3d33eed630b6422b
gKushagra <55290773+gKushagra@users.noreply.github.com>
2022-12-27 21:04:54 -0500

    fixed build minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-    "mode": "none",
-    "entry": {
-        main: [
-            "./src/components/sidebar.js",
-        ]
+    mode: isDevelopment ? 'development' : 'production',
+    output: {
+        filename: isDevelopment ? '[name].js' : '[name].[hash].js'
     },
-    "output": {
-        "path": __dirname + '/dist',
-        "filename": "[name].js"
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: !isDevelopment }
+                    }
+                ]
+            }
+        ]
     }
-};
+}
